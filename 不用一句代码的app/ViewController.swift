f6dcba2714c613af4d2a1d4b7f6e181374f70556
zhangxi <zhangxi_1989@sina.com>
2015-09-07 20:25:46 +0800

    init

---
+++ 不用一句代码的app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  不用一句代码的app
+//
+//  Created by 张 玺 on 15/1/31.
+//  Copyright (c) 2015年 me.zhangxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
